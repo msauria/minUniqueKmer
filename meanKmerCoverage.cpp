f0c94def827e1b8e3994ad01ec4632550460e024
msauria <crockopotamus@gmail.com>
2021-11-22 13:31:14 -0500

    Added kmer analysis scripts

---
+++ meanKmerCoverage.cpp
@@ -0,0 +1,171 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+size_t Nchr=0;
+vector<size_t> cindices;
+vector<uint8_t> mul, mur;
+vector<std::string> cnames;
+
+void print_info(void)
+{
+  std::cout << "Find mean unique k-mer coverage for a given k-mer size at each genomic position ver. 1.0\n"
+        << "\nUsage:\nmeanKmerCoverage <k-mer> <chrom_sz> <mul_wig> <mur_wig> <out_wig>\n"
+        << "Parameters:\n"
+        << "<k-mer>         - Maximum unique k-mer size\n"
+        << "<chrom_sz>      - Two-column tab-separated chromosome size file\n"
+        << "<mul_wig>       - Left-anchored minimum unique k-mer wiggle file\n"
+        << "<mur_wig>       - Right-anchored minimum unique k-mer wiggle file\n"
+        << "<out_wig>       - Output mean k-mer coverage wiggle file\n";
+}
+
+bool help(int argc, char** argv)
+{
+  const std::string help = "--help";
+  for (int i = 1; i < argc; ++i)
+  {
+    if (argv[i] == help)
+      return true;
+  }
+  return false;
+}
+
+void load_wiggle(char* fname, uint8_t kmer, std::vector<uint8_t>* wig, bool forward)
+{
+  ifstream input(fname);
+  string cur, field, chrom;
+  size_t cstart, pos, split, val;
+  while (getline(input, cur))
+  {
+    if (cur.find("f") == 0)
+    {
+      while (cur.length() > 0)
+      {
+        split = cur.find(" ");
+        if (split == std::string::npos)
+          split = cur.length();
+        field = cur.substr(0, split);
+        if (split == cur.length())
+          cur.erase(0, split);
+        else
+          cur.erase(0, split + 1);
+        if (field.find("chrom") == 0)
+          chrom = field.substr(field.find("=") + 1, field.length());
+        else if (field.find("start") == 0)
+          pos = stoi(field.substr(field.find("=") + 1, field.length())) - 1;
+      }
+      size_t index;
+      for (index=0; index<Nchr; index++)
+        if (cnames[index].compare(chrom) == 0)
+          break;
+      cstart = cindices[index];
+      pos += cstart;
+    } else {
+      val = stoi(cur);
+      if (val <= kmer)
+      {
+        if (forward)
+          for (size_t i=0; i<kmer; i++)
+            (*wig)[pos+i]++;
+        else
+          for (size_t i=0; i<kmer; i++)
+            (*wig)[pos-i]++;
+      }
+      pos++;
+    }
+  }
+  input.close();
+}
+
+std::string rtrim(const std::string &s)
+{
+    size_t end = s.find_last_not_of("0");
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+
+int main(int argc, char *argv[])
+{
+  fprintf(stderr, "\r                                     \r");
+  fflush(stderr);
+  if (argc <= 5 || help(argc, argv))
+  {
+    print_info();
+    return 1;
+  }
+
+  uint8_t Kmer = stoi(argv[1]);
+  float Kmer2=Kmer*2;
+
+  // Load chromosome sizes
+  ifstream input(argv[2]);
+  cindices.resize(0);
+  cnames.resize(0);
+  string cur;
+  size_t pos=0;
+  cindices.push_back(pos);
+  fprintf(stderr, "\rReading chromosome sizes ");
+  fflush(stderr);
+  while (getline(input, cur))
+  {
+    size_t name_stop = cur.find("\t");
+    pos += std::stoi(cur.substr(name_stop + 1));
+    Nchr += 1;
+    cnames.push_back(cur.substr(0, name_stop));
+    cindices.push_back(pos);
+  }
+  input.close();
+
+  // Load wiggle files
+  fprintf(stderr, "\r                         \r");
+  fprintf(stderr, "\rAllocating MUL array ");
+  fflush(stderr);
+  mul.resize(cindices[Nchr]);
+  fprintf(stderr, "\r                         \r");
+  fprintf(stderr, "\rLoading MUL array ");
+  fflush(stderr);
+  load_wiggle(argv[3], Kmer, &mul, true);
+  fprintf(stderr, "\r                         \r");
+  fprintf(stderr, "\rAllocating MUR array ");
+  fflush(stderr);
+  mur.resize(cindices[Nchr]);
+  fprintf(stderr, "\r                         \r");
+  fprintf(stderr, "\rLoading MUR array ");
+  fflush(stderr);
+  load_wiggle(argv[4], Kmer, &mur, false);
+
+  // Write minimum unique fragment wiggle
+  fprintf(stderr, "\r                         \r");
+  fprintf(stderr, "\rWriting output ");
+  fflush(stderr);
+  ofstream outfile;
+  outfile.open (argv[5]);
+  bool prev;
+  std::string val;
+  for (size_t i=0; i<Nchr; i++)
+  {
+    prev = false;
+    for (size_t j=cindices[i]; j<cindices[i+1]; j++)
+    {
+      if (mul[j] == 0 && mur[j] == 0)
+      {
+        prev = false;
+        continue;
+      }
+      val = to_string((mur[j] + mul[j]) / Kmer2);
+      val = rtrim(val);
+      if (!prev) outfile << "fixedStep chrom=" + cnames[i] + " start=" +
+                            to_string(j - cindices[i] + 1) + " step=1\n";
+      outfile << val + "\n";
+      prev = true;
+    }
+  }
+  outfile.close();
+  fprintf(stderr, "\r                         \r");
+  fflush(stderr);
+  return EXIT_SUCCESS;
+}
